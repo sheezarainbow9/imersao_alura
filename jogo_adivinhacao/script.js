74ace1b8ec43e745a0951f4d39113b65f42d644b
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-01-31 13:22:51 -0300

    Correção de código

--- jogo_adivinhacao/script.js
+++ jogo_adivinhacao/script.js
@@ -1,18 +1,18 @@
-/* Math.random() vai escolher um número (inteiro) aleatoriamente de 0 até 999 aqui:*/
-var numeroSecreto = parseInt(Math.random() * 1001);
-var tentativa = 3;
+/* Math.random() vai escolher um número (inteiro) aleatoriamente de 0 até 19 aqui:*/
+var numeroSecreto = parseInt(Math.random() * 21);
+var tentativa = 7;
 
 while (chute != numeroSecreto) {
-  var chute = parseInt(prompt("Digite um número entre 1 e 1000: "));
-  if (chute < 0 || chute > 1000) {
-    alert("Válido somente número entre 0 e 1000.");
+  var chute = parseInt(prompt("Digite um número entre 1 e 20: "));
+  if (chute < 0 || chute > 20) {
+    alert("Válido somente número entre 0 e 20.");
   } else if (chute == numeroSecreto) {
     alert(`Parabéns, você acertou!`);
     break;
   } else if (chute > numeroSecreto) {
-    alert("Errou! O número secreto é menor!");
+    alert(`Errou! O número secreto é menor! Você tem mais ${tentativa-1} tentativas.`);
   } else if (chute < numeroSecreto) {
-    alert("Errou! O número secreto é maior!");
+    alert(`Errou! O número secreto é maior! Você tem mais ${tentativa-1} tentativas.`);
   }
   -- tentativa
   if (tentativa < 1) {
