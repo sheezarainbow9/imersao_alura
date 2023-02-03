b4ae6f6bd4ed78496a18bebc59586b364c1f613c
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-02-03 11:47:33 -0300

    tabela_classificacao

---
+++ tabela_classificacao/script.js
@@ -0,0 +1,15 @@
+var paulo = {
+  nome: "Paulo",
+  vitoria: 0,
+  empate: 0,
+  derrota: 0,
+  pontos: 0,
+};
+
+alert(paulo.vitoria);
+
+function adicionarVitoria() {}
+
+function adicionarEmpate() {}
+
+function adicionarDerrota() {}
